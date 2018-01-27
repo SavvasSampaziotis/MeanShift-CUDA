d4572e958729f73361d127275339b921fa7cf825
Savvas Sampaziotis <savvas456pentium@gmail.com>
2018-01-27 08:55:26 +0200

    Minor Improvements in MATLAB

--- matlab/read_meanshift_output.m
+++ matlab/read_meanshift_output.m
@@ -8,19 +8,18 @@ D = fread( fileID, 1, 'int32' );
 
 data = fread( fileID, N*D, 'float');
 
-% data = [data(1:2:end), data(2:2:end)];
+% data = [data(1:2:end), da ta(2:2:end)];
 
-data = reshape(data, N, D)';
+data = reshape(data, N, D);
 
 fclose(fileID);
 
 
+load('../data/meanshift_result.mat');
+figure(1); clf; hold on;
+scatter(y(:,1),y(:,2));
+scatter(data(:,1),data(:,2));
+hold off;
 
-% load('../data/meanshift_result.mat');
-% figure(1); clf; hold on;
-% scatter(y(:,1),y(:,2));
-% scatter(data(:,1),data(:,2));
-% hold off;
-% 
-% E = mean((y- data).^2);
-% E
+E = mean((y- data).^2);
+E
